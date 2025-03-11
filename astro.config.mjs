52eecde734edefa708aaaf5dc9f2742ab6bfce61
asieradzk <bluekewe94@gmail.com>
2025-03-11 13:31:52 +0100

    Added a short article about TorchSharp and Unity.

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,6 +37,12 @@ export default defineConfig({
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+				{
+					label: 'Other',
+					items: [
+						{label: 'TorchSharp in Unity', slug: 'other/torchsharpunity'},
+					]
+				}
 			],
 		}),
 	],
